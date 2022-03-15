09036dd5e05811b5df22a1100e2078b6307f841f
poeyik <crd9709@hansung.ac.kr>
2022-03-15 23:19:29 +0900

    220315

--- src/main/java/spring/core/member/MemberServiceImpl.java
+++ src/main/java/spring/core/member/MemberServiceImpl.java
@@ -8,6 +8,7 @@ public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+
     @Autowired //ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
